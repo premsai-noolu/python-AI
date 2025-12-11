2746766307bcf755ce0371ff856d6398c1ba9f1f
premsai-noolu <premsainoolu01@gmail.com>
2025-12-11 10:40:33 +0530

    self arguments

---
+++ OOPS/self_arguments.py
@@ -0,0 +1,14 @@
+class chaicup:
+    size = 150 #ml
+
+    def describe(self):
+        return f"A {self.size}ml chai cup"
+    
+
+cup = chaicup()
+print(cup.describe())
+print(chaicup.describe(cup))
+
+cup_two=chaicup()
+cup_two.size=200
+print(cup_two.describe())
